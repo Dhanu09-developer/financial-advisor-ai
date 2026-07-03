c18a59bdace0fab81cfe65f9ae15365afa00d9dc
dhanashree509 <dhanashreehodage5@gmail.com>
2026-07-03 23:17:15 +0530

    Initial project setup

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Financial Research AI Agent")
+
+st.write("Hello Buddy!")
+
+st.write("Myy First Streamlit application is running successfully")
---
+++ test_package.py
@@ -0,0 +1,7 @@
+import streamlit
+import pandas
+import yfinance
+import plotly
+import langchain
+
+print("All packages imported successfully")
