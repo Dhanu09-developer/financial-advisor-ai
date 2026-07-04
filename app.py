f817d0e03662150cad3e6dd8ceac7c9ac3e5f9c6
dhanashree509 <dhanashreehodage5@gmail.com>
2026-07-04 19:43:21 +0530

    Complete Step 4 - Added Stock Chart Visualization

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
 import streamlit as st
+import yfinance as yf
 
-st.title("Financial Research AI Agent")
+st.title("💰 Stock Price ChatBot")
 
-st.write("Hello Buddy!")
+symbol = st.text_input("Enter Stock Symbol", "RELIANCE.NS")
 
-st.write("Myy First Streamlit application is running successfully")
+if symbol:
+    stock = yf.Ticker(symbol)
+    
+    data = stock.history(period="1d")
+    if not data.empty:
+
+        current_price = data['Close'].iloc[-1]
+        st.success(f"Current Price: ₹{current_price:.2f}")
+
+    else:
+        st.warning("No data found for the given symbol.")
+
+    history = stock.history(period="1mo")
+    st.subheader("Last 1 Month Stock Performance")
+    
+    st.line_chart(history['Close'])
+    
